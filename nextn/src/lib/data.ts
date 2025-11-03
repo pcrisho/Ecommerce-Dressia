4137e954486ba4f4888e64c4b03743c5490b0d55
Paolo Crisóstomo <paolo_crisostomo@outlook.com>
2025-11-03 12:41:32 -0500

    add resources

--- nextn/src/lib/data.ts
+++ nextn/src/lib/data.ts
@@ -4,40 +4,34 @@ import { PlaceHolderImages } from './placeholder-images';
 const dressImages = PlaceHolderImages.filter(img => img.id.startsWith('dress-'));
 
 export const vestidos: Dress[] = [
-  { 
-    id: '1', 
-    nombre: 'Vestido "Serenidad"', 
-    precio: 120, 
-    image: dressImages.find(i => i.id === 'dress-1')! 
+  {
+    id: '1',
+    nombre: 'Blusa "Bembella White"',
+    precio: 120,
+    image: dressImages.find(i => i.id === 'dress-1')!
   },
-  { 
-    id: '2', 
-    nombre: 'Vestido "Noche"', 
-    precio: 180, 
-    image: dressImages.find(i => i.id === 'dress-2')! 
+  {
+    id: '2',
+    nombre: 'Blusa "Bembella"',
+    precio: 180,
+    image: dressImages.find(i => i.id === 'dress-2')!
   },
-  { 
-    id: '3', 
-    nombre: 'Vestido "Aura"', 
-    precio: 150, 
-    image: dressImages.find(i => i.id === 'dress-3')! 
+  {
+    id: '3',
+    nombre: 'Blusa "Malva"',
+    precio: 150,
+    image: dressImages.find(i => i.id === 'dress-3')!
   },
-  { 
-    id: '4', 
-    nombre: 'Vestido "Brisa"', 
-    precio: 95, 
-    image: dressImages.find(i => i.id === 'dress-4')! 
+  {
+    id: '4',
+    nombre: 'Vestido "Matchy"',
+    precio: 95,
+    image: dressImages.find(i => i.id === 'dress-4')!
   },
-  { 
-    id: '5', 
-    nombre: 'Vestido "Eclipse"', 
-    precio: 135, 
-    image: dressImages.find(i => i.id === 'dress-5')! 
-  },
-  { 
-    id: '6', 
-    nombre: 'Vestido "Dorado"', 
-    precio: 250, 
-    image: dressImages.find(i => i.id === 'dress-6')! 
+  {
+    id: '5',
+    nombre: 'Vestido "Topp"',
+    precio: 135,
+    image: dressImages.find(i => i.id === 'dress-5')!
   },
 ];
